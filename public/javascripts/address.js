138dde1fe5b1984398c763d901dfb136100caf15
cponeill <caseyoneill78@hotmail.com>
2014-09-06 13:43:50 -0700

    adding code

--- public/javascripts/address.js
+++ public/javascripts/address.js
@@ -8,28 +8,28 @@ $(document).ready(function(){ // Enable jQuery.
 				xhr.setRequestHeader ('Authorization', 'Basic '+btoa('05e17b4bd6bf6ecd3df2947c027ce523')); // Authorizing the API call.
 			},
 			success: function(data) { // If the API call is successfull, it assigns the JSON to this function.
-				var BTChash = data.hash.toUpperCase();
-        		var BTCBalance = data.balance / 100000000.0;
-        		var BTCSent = data.sent / 100000000.0;
-        		var BTCReceived = data.received / 100000000.0;
-        		var BTCuncomfirmed = data.unconfirmed_balance / 100000000.0;
+                                var BTChash = data.hash.toUpperCase();
+                                var BTCBalance = data.balance / 100000000.0;
+        		        var BTCSent = data.sent / 100000000.0;
+        		        var BTCReceived = data.received / 100000000.0;
+        		        var BTCuncomfirmed = data.unconfirmed_balance / 100000000.0;
 
-        		if (BTCuncomfirmed <= 0) {
-        			$('#uncomfirm').append("No Uncomfirmed Coins");
-        		} else {
-        			$('#uncomfirm').append(BTCuncomfirmed);
-        		}
+                                if (BTCuncomfirmed <= 0) {
+                                        $('#uncomfirm').append("No Uncomfirmed Coins");
+                                } else {
+        			        $('#uncomfirm').append(BTCuncomfirmed);
+        		        }
 
-        		$('#Address').append('<h3>Address</h3>');
-        		$('#Sent').append('<h3>Sent</h3>');
-        		$('#Received').append('<h3>Received</h3>');
-        		$('#Balance').append('<h3>Balance</h3>');
-        		$('#Uncomfirmed').append('<h3>Uncomfirmed</h3>');
+        		        $('#Address').append('<h3>Address</h3>');
+        		        $('#Sent').append('<h3>Sent</h3>');
+        		        $('#Received').append('<h3>Received</h3>');
+        		        $('#Balance').append('<h3>Balance</h3>');
+        		        $('#Uncomfirmed').append('<h3>Uncomfirmed</h3>');
 
-        		$('#btcAddress').append(BTChash);
-        		$('#balance').append("<img src='/images/BitMoney.png' /> " + BTCBalance);
-        		$('#sent').append("<img src='/images/BitMoney.png' /> " + BTCSent);
-        		$('#received').append("<img src='/images/BitMoney.png' /> " + BTCReceived);
+        		        $('#btcAddress').append(BTChash);
+        		        $('#balance').append("<img src='/images/BitMoney.png' /> " + BTCBalance);
+        		        $('#sent').append("<img src='/images/BitMoney.png' /> " + BTCSent);
+        		        $('#received').append("<img src='/images/BitMoney.png' /> " + BTCReceived);
 
 			},
 			error: function(req, msg, err) {
